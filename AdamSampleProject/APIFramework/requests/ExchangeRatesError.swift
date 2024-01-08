bdc460812f6d3cc6bcde44a81634a5cea2087cf8
Adam Johnston <Adam.Johnston@officetrax.com>
2024-01-08 10:39:05 +0000

    small fixes

--- AdamSampleProject/APIFramework/requests/ExchangeRatesError.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// Dumb error to model simple errors
-/// In a real implementation this should be more exhaustive
-public enum ExchangeRatesError: Error {
-	case encoding
-	case decoding
-	case server(message: String)
-}
